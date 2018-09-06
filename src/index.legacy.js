c6a13ac0c7be631a5ab003c6a86c48fe37e86017
Stefan Born <stefan.born@phzh.ch>
2018-09-06 08:02:28 +0200

    legacy bundle modified for custom build just with sortable

--- src/index.legacy.js
+++ src/index.legacy.js
@@ -8,20 +8,20 @@ import AbstractEvent from 'shared/AbstractEvent';
 import AbstractPlugin from 'shared/AbstractPlugin';
 
 import * as Sensors from './Draggable/Sensors';
-import * as Plugins from './Plugins';
+//import * as Plugins from './Plugins';
 
 import Draggable from './Draggable';
-import Droppable from './Droppable';
-import Swappable from './Swappable';
+//import Droppable from './Droppable';
+//import Swappable from './Swappable';
 import Sortable from './Sortable';
 
 export {
   AbstractEvent as BaseEvent,
   AbstractPlugin as BasePlugin,
   Draggable,
-  Droppable,
-  Swappable,
+  //Droppable,
+  //Swappable,
   Sortable,
   Sensors,
-  Plugins,
+  //Plugins,
 };
